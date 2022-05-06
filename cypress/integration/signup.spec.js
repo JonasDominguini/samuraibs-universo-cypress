12559b2ef60d57ece45f753a6c2286c782605764
Jonas Dominguini De Souza <jonasdominguini@gmail.com>
2022-05-05 23:39:12 -0300

    71, Usando Fixture p/ extterna a massa de testes

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,24 +1,26 @@
-
 import faker from "@faker-js/faker";
 import signupPage from "../support/pages/signup/";
 
-describe("Cadastro", () => {
-  context("Quando o usuário é novato", () => {
-    const user = {
-      name: "Jonas Dominguini",
-      email: "jonas@samuraibs.com",
-      password: "pwd123",
-    };
+describe("Cadastro", function () {
+  before(function () {
+    cy.fixture("signup").then(function (signup) {
+      this.success = signup.success;
+      this.email_dup = signup.email_dup;
+      this.email_inv = signup.email_inv;
+      this.short_password = signup.short_password;
+    });
+  });
 
+  context("Quando o usuário é novato", function () {
     before(function () {
-      cy.task("removeUser", user.email).then(function (result) {
+      cy.task("removeUser", this.success.email).then(function (result) {
         console.log(result);
       });
     });
 
-    it("Deve cadastrar com sucesso", () => {
+    it("Deve cadastrar com sucesso", function () {
       signupPage.go();
-      signupPage.form(user);
+      signupPage.form(this.success);
       signupPage.submit();
       signupPage.toast.shouldtHaveText(
         "Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!"
@@ -26,70 +28,42 @@ describe("Cadastro", () => {
     });
   });
 
-  context("Quando o email já existe", () => {
-    const user = {
-      name: "João Lucas",
-      email: "joão@samuraibs.com",
-      password: "pwd123",
-      is_provider: true,
-    };
+  context("Quando o email já existe", function () {
     before(function () {
-      cy.task("removeUser", user.email).then(function (result) {
-        console.log(result);
-      });
-
-      cy.request("POST", "http://localhost:3333/users", user).then(function (
-        response
-      ) {
-        expect(response.status).to.eq(200);
-      });
+      cy.postUser(this.email_dup);
     });
 
-    it("Deve exibir email já cadastrado", () => {
-      cy.visit("/signup");
-      cy.get('input[placeholder^="Nome"]').type(user.name);
-      cy.get('input[placeholder$="email"]').type(user.email);
-      cy.get('input[placeholder*="senha"]').type(user.password);
-      cy.contains("button", "Cadastrar").click();
-
-      cy.get(".toast")
-        .should("be.visible")
-        .find("p")
-        .should("have.text", "Email já cadastrado para outro usuário.");
+    it("Deve exibir email já cadastrado", function () {
+      signupPage.go();
+      signupPage.form(this.email_dup);
+      signupPage.toast.shouldtHaveText(
+        "Email já cadastrado para outro usuário."
+      );
+      
     });
   });
 
-  context("Quando email está incorreto", () => {
-    const user = {
-      name: "Elizabeth Olsen",
-      email: "liza.yahoo.com",
-      password: "pwd123",
-    };
-
-    it("Deve exibir mensagem de alerta", () => {
+  context("Quando email está incorreto", function () {
+    it("Deve exibir mensagem de alerta", function () {
       signupPage.go();
-      signupPage.form(user);
+      signupPage.form(this.email_inv);
       signupPage.submit();
       signupPage.alertHaveText("Informe um email válido");
     });
   });
 
-  context("Quando a senha é muito curta", () => {
-    const password = ["1", "2a", "3a$", "abc4", "ab#c5", "adf7*6"];
+  context("Quando a senha é muito curta", function () {
+    const password = ["1", "2a", "3a$", "abc4", "ab#c5", ];
 
-    beforeEach(() => {
+    beforeEach(function () {
       signupPage.go();
     });
 
     password.forEach(function (p) {
       it("Não deve cadastrar com a senha: " + p, function () {
-        const user = {
-          name: "Janson Friday",
-          email: "jason@gamil.com",
-          password: p,
-        };
+        this.short_password.password = p;
 
-        signupPage.form(user);
+        signupPage.form(this.short_password);
         signupPage.submit();
       });
     });
