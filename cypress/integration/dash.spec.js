32139857b30e818853020ade26a7a3ba2595f0eb
Jonas Dominguini De Souza <jonasdominguini@gmail.com>
2022-07-17 21:55:24 -0300

    Alterações sendo colocadas em select.day

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,8 +1,6 @@
 const {
   getCypressElementCoordinates,
 } = require("cypress-real-events/getCypressElementCoordinates");
-
-import loginPage from "../support/pages/login";
 import dashPage from "../support/pages/dashboard";
 
 import { customer, provider, appointment } from "../support/factories/dash";
@@ -18,76 +16,14 @@ describe("dashboard", function () {
       cy.createAppointment(appointment.hour);
     });
     it("O mesmo deve ser exibido no dashboard", function () {
-      loginPage.go();
-      loginPage.form(provider);
-      loginPage.submit();
-      dashPage.calendarShoulBeVisible();
-
       const day = Cypress.env("appointmentDay");
+
+      cy.apiLogin(provider, true);
+      //cy.uiLogin(provider);
+
+      dashPage.calendarShoulBeVisible();
       dashPage.selectDay(day);
       dashPage.appointmentShouldBe(customer, appointment.hour);
     });
-  });
-});
-
-import moment from "moment";
-
-Cypress.Commands.add("setProviderId", function (providerEmail) {
-  cy.request({
-    method: "GET",
-    url: "http://localhost:3333/providers",
-    headers: {
-      authorization: "Bearer " + Cypress.env("apiToken"),
-    },
-  }).then(function (response) {
-    expect(response.status).to.eq(200);
-    console.log(response.body);
-
-    const providerlist = response.body;
-
-    providerlist.forEach(function (provider) {
-      if (provider.email === providerEmail)
-        Cypress.env("providerId", provider.id);
-    });
-  });
-});
-
-Cypress.Commands.add("createAppointment", function (hour) {
-  let now = new Date();
-  now.setDate(now.getDate() + 1);
-
-  Cypress.env("appointmentDay", now.getDate());
-
-  const date = moment(now).format("YYYY-MM-DD " + hour);
-
-  const payload = {
-    provider_id: Cypress.env("providerId"),
-    date: date,
-  };
-  cy.request({
-    method: "POST",
-    url: "http://localhost:3333/appointments",
-    body: payload,
-    headers: {
-      authorization: "Bearer " + Cypress.env("apiToken"),
-    },
-    failOnStatusCode: false,
-  }).then(function (response) {
-    expect(response.status).to.eq(200);
-  });
-});
-
-Cypress.Commands.add("apiLogin", function (user) {
-  const payload = {
-    email: user.email,
-    password: user.password,
-  };
-  cy.request({
-    method: "POST",
-    url: "http://localhost:3333/sessions",
-    body: payload,
-  }).then(function (response) {
-    expect(response.status).to.eq(200);
-    Cypress.env("apiToken", response.body.token);
   });
 });
--- cypress/support/pages/dashboard/index.js
+++ cypress/support/pages/dashboard/index.js
@@ -6,18 +6,35 @@ class DashPage {
   }
 
   calendarShoulBeVisible() {
-    cy.get(".DayPicker", { timeout: 9000 }).should("be.visible");
+    cy.get(el.calendar, { timeout: 9000 }).should("be.visible");
   }
   selectDay(day) {
+    let today = new Date();
+    let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+    if (today.getDate() === lastDayOfMonth.getDate()) {
+      cy.log("Hoje é o último dia do mês");
+
+      cy.get(el.nextMonthButton).should("be.visible").click();
+
+      // Isso é um checkpoint para garantir que houve a troca do calendário
+      cy.contains(el.monthYearName, "Abril").should("be.visible");
+    } else {
+      cy.log("Hoje não é o último dia do mês");
+    }
+
+    cy.log(today.toString());
+    cy.log(lastDayOfMonth.toString());
+
     const target = new RegExp("^" + day + "$", "g");
-    cy.contains(".DayPicker-Day--available", target).click({force:true});
+    cy.contains(el.boxDay, target).click({ force: true });
   }
 
   appointmentShouldBe(customer, hour) {
     cy.contains("div", customer.name)
       .should("be.visible")
       .parent()
-      .contains('span[class="appointment"]', hour)
+      .contains(el.boxHour, hour)
       .should("be.visible");
   }
 }
