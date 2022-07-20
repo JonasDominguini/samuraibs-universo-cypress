b5284ad197534a0d3a16a2df59e1a5acc34c8ee5
Jonas Dominguini De Souza <jonasdominguini@gmail.com>
2022-07-19 23:22:57 -0300

    Colocado timeout implicito de 30s, devido ao serve

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -2,9 +2,9 @@ import { el } from "./elements";
 
 class Toast {
   shouldtHaveText(expectText) {
-    cy.get(el.toast, { timeout: 10000 })
+    cy.get(el.toast, { timeout: 10000 })// timeout que deixei, diferente do que manda em aula. Não quero os 30 s indicados como padrão.
       .should("be.visible")
-      .should("have.css", "opacity", "1", { timeout: 1500 })
+      .should("have.css", "opacity", "1", { timeout: 2000 })
       .find("p")
       .should("have.text", expectText);
   }
